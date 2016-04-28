35f4f9f3d1405d448bcc634522d6df2a6fafbc94
isaac <isaacg311@gmail.com>
2016-04-27 23:06:53 -0430

    Cree archivo tarea2.py

---
+++ tarea2.py
@@ -0,0 +1,15 @@
+'''
+Isaac González 11-10396
+Kervyn Rivero 11-10874
+Equipo: Onesoft
+Tarea 2'''
+
+#Clase Tarifa
+class Tarifa(object):
+
+    def __init__(self)
+        self.semana = 1
+        self.finsemana = 2
+
+def calcularPrecio (self,tarifa, tiempoDeTrabajo):
+    
